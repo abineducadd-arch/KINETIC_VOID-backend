9f6b9622ba02c7e882bf82ee0bf3021842caad67
abineducadd-arch <abineducadd@gmail.com>
2026-04-11 00:24:55 +0530

    final

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-set -o errexit      # Exit immediately if any command fails
-pip install -r requirements.txt   # Install all dependencies
-python manage.py collectstatic --no-input   # Gather static files (CSS, JS, images)
-python manage.py migrate            # Apply database migrations
+set -o errexit
+
+pip install -r requirements.txt
+
+cd backend   # <-- add this line
+
+python manage.py collectstatic --no-input
+python manage.py migrate --noinput
